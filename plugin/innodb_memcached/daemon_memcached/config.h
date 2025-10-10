07f0ffc039f7056a1a5c55185fec8f3d5eb263e4
xiebaoma <2402583447@qq.com>
2025-10-10 09:37:40 +0800

    plugin

---
+++ plugin/innodb_memcached/daemon_memcached/config.h
@@ -0,0 +1,11 @@
+/*
+  This file was auto-generated in the original distribution. We don't
+  want to use it, and instead we will use MySQL's one, plus some
+  static configuration that the original file included. This file is
+  left as deleting it would require changes in many files that will
+  depart from the original distribution even more.
+*/
+
+#include "my_config.h"
+
+#include "config_static.h"
---
+++ plugin/innodb_memcached/innodb_memcache/config.h
@@ -0,0 +1,8 @@
+/*
+  This file was auto-generated in the original distribution. We don't
+  want to use it, and instead we will use MySQL's one. This file is
+  left as deleting it would require changes in many files that will
+  depart from the original distribution even more.
+*/
+
+#include "my_config.h"
